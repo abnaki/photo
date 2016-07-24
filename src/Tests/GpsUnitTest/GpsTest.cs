804ef5f5c6a7341c1854b41bfc0edb6269bf5d81
Abnaki Light Industry <abnaki@users.noreply.github.com>
2016-07-24 10:17:53 -0400

    Arrays of URational or Rational are supplied to ExifItem.Value by Converter (#1)
    * Converter.cs create array of URational or Rational when necessary for GPS fields
    
    * Abnaki.Fork prefix in AssemblyName, as distinct from original
    
    * Removed .nuget
    
    * Added MS flavor of GpsUnitTest project.
    
    * Only paid attention to photo.exif project, not others.

---
+++ src/Tests/GpsUnitTest/GpsTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using photo.exif;
+
+namespace GpsUnitTest
+{
+    [TestClass]
+    public class GpsTest
+    {
+        [TestMethod]
+        public void TestImage()
+        {
+            FileInfo fi = new FileInfo(Path.Combine(Environment.CurrentDirectory,
+                @"..\..\..\..\..\Images\Samsung SIII.jpg")); // contains GPS coordinates
+
+            Parser par = new Parser();
+
+            // id 1 through 4 have GPS data
+            IEnumerable<ExifItem> gpsItems = par.Parse(fi.FullName).Where(x => 1 <= x.Id && x.Id <= 4).ToList();
+
+            Debug.Assert(gpsItems.Any());
+
+            foreach ( ExifItem x in gpsItems )
+            {
+                Debug.WriteLine(x);
+                Debug.WriteLine("");
+            }
+
+            // latitude and longitude arrays
+            IEnumerable<ExifItem> arrayItems = gpsItems.Where(x => x.Id == 2 || x.Id == 4);
+            foreach ( ExifItem x in arrayItems )
+            {
+                Debug.Assert(x.Value is URational[]);
+            }
+        }
+    }
+}
--- src/photo.exif/Converter.cs
+++ src/photo.exif/Converter.cs
@@ -45,11 +45,11 @@ namespace photo.exif
                 case ExifType.UInt32:
                     return BitConverter.ToUInt32(bytes.Safe(4), 0);
                 case ExifType.URational:
-                    return new URational
-                               {
-                                   Denominator = BitConverter.ToUInt32(bytes, 4),
-                                   Numerator = BitConverter.ToUInt32(bytes, 0)
-                               };
+                    if (len > 8)
+                        return URationalArray(bytes, len);
+                    else
+                        return URationalFromBytes(bytes, 0);
+
                 case ExifType.Object:
                     return bytes;
                 case ExifType.Int32:
@@ -57,15 +57,46 @@ namespace photo.exif
                 case ExifType.Long:
                     return BitConverter.ToInt64(bytes.Safe(8), 0);
                 case ExifType.Rational:
-                    return new Rational
-                               {
-                                   Denominator = BitConverter.ToInt32(bytes, 0),
-                                   Numerator = BitConverter.ToInt32(bytes, 4)
-                               };
+                    if (len > 8)
+                        return RationalArray(bytes, len);
+                    else
+                        return RationalFromBytes(bytes, 0);
 
                 default:
                     return bytes;
             }
+        }
+
+        static URational URationalFromBytes(byte[] bytes, int offset)
+        {
+            return new URational
+            {
+                Denominator = BitConverter.ToUInt32(bytes, offset + 4),
+                Numerator = BitConverter.ToUInt32(bytes, offset)
+            };
+        }
+
+        static URational[] URationalArray(byte[] bytes, int overallLength)
+        {
+            int n = overallLength / 8;
+
+            return Enumerable.Range(0, n).Select(k => URationalFromBytes(bytes, 8 * k)).ToArray();
+        }
+
+        static Rational RationalFromBytes(byte[] bytes, int offset)
+        {
+            return new Rational
+            {
+                Denominator = BitConverter.ToInt32(bytes, 0),
+                Numerator = BitConverter.ToInt32(bytes, 4)
+            };
+        }
+
+        static Rational[] RationalArray(byte[] bytes, int overallLength)
+        {
+            int n = overallLength / 8;
+
+            return Enumerable.Range(0, n).Select(k => RationalFromBytes(bytes, 8 * k)).ToArray();
         }
 
         public static byte[] Safe(this byte[] bytes, int minimun)
