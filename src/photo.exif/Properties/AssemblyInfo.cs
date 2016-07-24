3cb54979d68cfe00c29727343bc313e382d0e0b7
abnaki <abnaki>
2016-07-24 10:25:31 -0400

    Appopriate for fork

--- src/photo.exif/Properties/AssemblyInfo.cs
+++ src/photo.exif/Properties/AssemblyInfo.cs
@@ -2,13 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Photo Exif")]
-[assembly: AssemblyDescription("Reads EXIF data (exchangeable image file format) from image files")]
+// General Information about an assembly 
+[assembly: AssemblyTitle("Photo Exif, Abnaki fork")]
+[assembly: AssemblyDescription("Originally by Oscar Fraxedas.  Reads EXIF data (exchangeable image file format) from image files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Oscar Fraxedas")]
+[assembly: AssemblyCompany("Abnaki Light Industry LLC")]
 [assembly: AssemblyProduct("photo.exif")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -19,9 +17,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("353e59d2-03f9-40e3-833a-33e272a03c01")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -29,8 +24,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("20.1.0.0")]
+[assembly: AssemblyFileVersion("20.1.0.0")]
